55c6ae10382965a01e8f2dbe825f65edf8f4a440
Ilya Sirosh <sirosh.ilya@gmail.com>
2021-02-12 15:12:52 +0200

    Initial Commit

---
+++ PhotoWidget/PhotoWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoWidgetApp.swift
+//  PhotoWidget
+//
+//  Created by Illya Sirosh on 12.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct PhotoWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
